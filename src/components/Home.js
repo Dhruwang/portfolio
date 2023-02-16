c2d21fbcb6dbaf05da55cd530a1b8c9be9e91544
Dhruwang <dhruwangjariwala18@gmail.com>
2023-02-16 09:43:18 +0530

    added laptop

---
+++ src/components/Home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div className='home'>
+            <div className='laptopContainer'>
+                <div className='laptopContainerInner'>
+                    <div class="laptop">
+                        <div className='laptopTop'>
+                            <div className='laptopTopInner'>
+                                <div className='notch'></div>
+                            </div>
+                        </div>
+                        <div className='laptopBottom'></div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
