6446849abfb2aa697b1714a78b16b3cee9d88f13
Dhruwang <dhruwangjariwala18@gmail.com>
2023-03-08 23:56:57 +0530

    made responsive for tablet size devices

--- src/App.js
+++ src/App.js
@@ -14,10 +14,8 @@ function App() {
   window.onscroll = function () { myFunction() };
 
   function myFunction() {
-    console.log(window.scrollY) 
     document.getElementById("spinner").style.top = `${window.scrollY>500?0:-100 + 0.2*window.scrollY}vh`
     document.getElementById("spinner").style.left = `${window.scrollY>600?0:60 - 0.1*window.scrollY}vw`
-    console.log(scrollHeight)
     if(window.scrollY<(1.4*scrollHeight)){
       document.getElementById("skillsInner").style.transform = "rotate3d(1, 0, 0, 90deg)"
     }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Link } from 'react-router-dom'
+import Spinner1 from "../images/spinner1.png"
+import Spinner2 from "../images/spinner2.png"
 
 
 
 export default function Home() {
+
+    const [themeCount, setthemeCount] = useState(1)
+
+    const chanmgeTheme =()=>{
+      var r = document.querySelector(':root');
+      if(themeCount===1){
+        
+      r.style.setProperty('--primary', '#352C2D');
+      r.style.setProperty('--secondary', '#E1D5CD');
+      setthemeCount(2);
+      }
+      else{
+        r.style.setProperty('--primary', '#E1D5CD');
+      r.style.setProperty('--secondary', '#352C2D');
+      setthemeCount(1);
+      }
+      
+    }
+  
     return (
         <div className='home' id='home'>
             <div className='homeInner'>
@@ -21,16 +42,21 @@ export default function Home() {
                 </div>
                 <div className='homeMiddle'>
                     <div className='homeMiddleLeft'>
-                    <h1>Jariwala Dhruwang</h1>
-                    <p>Full stack web developer</p>
+                        <h1>Jariwala Dhruwang</h1>
+                        <p>Full stack web developer</p>
                     </div>
                     <div className='homeMiddleRight'>
+
+                            <button className='spinnerBtn' onClick={chanmgeTheme}>
+                                <div className='spinnerMask'>
+
+                                </div>
+
+                                <img className="spinner2" id='spinner2' src={themeCount === 1 ? Spinner2 : Spinner1}></img>
+                            </button>
                         
-                        {/* <div className='spinner'>
-                            <div className='spinnerInner'></div>
-                        </div> */}
                     </div>
-                    
+
                 </div>
             </div>
         </div>
