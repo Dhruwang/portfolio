f5abef06c65ee220c74b69838316eb9a036bc0d2
Dhruwang <dhruwangjariwala18@gmail.com>
2023-03-10 17:11:40 +0530

    changed about me, title and favicon

--- src/components/About.js
+++ src/components/About.js
@@ -36,7 +36,7 @@ export default function About() {
       <div className='aboutInner'>
 
       <h1>About Me</h1>
-      <p>I am a Full stack web developer specialised in building profession web apps which are production ready and fully optimised I am a full stack web developer specialised in building profession web apps which are production ready and fully optimised I am a full stack web developer specialised in building profession web apps which are production ready and fully optimised I am a full stack web developer <span className='skillText'>Skills</span> in building profession web apps which are production ready and fully optimised</p>
+      <p>As a full stack web developer, I have a passion for creating innovative and engaging digital experiences. With a deep understanding of both front-end and back-end development, I am able to bring projects to life from start to finish.I am constantly seeking new challenges and opportunities to expand my skills and expertise, and I am committed to staying up-to-date with the latest trends and best practices in the field. Whether working on complex enterprise applications or simple landing pages, I approach each project with a dedication to excellence and a focus on delivering results.</p>
       </div>
     <div className='about2'></div>
     </div>
